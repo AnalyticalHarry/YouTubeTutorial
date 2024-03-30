b65a5eb32daf88950bccf1dbc41eef6a49ec6d09
Hemant Thapa <104982185+AnalyticalHarry@users.noreply.github.com>
2024-03-30 12:21:33 +0000

    Update and rename monteCarloSimulation.py to MonteCarloSimulationForRandomInitialPath.py

--- Algortihms/MonteCarloSimulationForRandomInitialPath.py
+++ Algortihms/MonteCarloSimulationForRandomInitialPath.py
@@ -3,36 +3,34 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import yfinance as yf
 
-# download NVDA data
 nvda_data = yf.download('NVDA', period='5y')
 
-# daily returns
 nvda_data['Daily Return'] = nvda_data['Adj Close'].pct_change()
-
 # number of simulations and trading days
 n_simulations = 2000
+# one month outlooks
 n_days = 30
-
 # last closing price and the daily return average and standard deviation
-last_price = nvda_data['Adj Close'].iloc[-1]  # Changed to .iloc[-1]
+last_price = nvda_data['Adj Close'][-1]
 mean_daily_return = nvda_data['Daily Return'].mean()
 std_dev_daily_return = nvda_data['Daily Return'].std()
+# future stock price paths
+simulation_df = pd.DataFrame()
 
-# all price series
-all_price_series = []
-
-# generate simulations
 for x in range(n_simulations):
+    count = 0
     daily_volatility = np.random.normal(mean_daily_return, std_dev_daily_return, n_days)
-    price_series = [last_price]  # Start with the last closing price
+    price_series = []
+    price = last_price * (1 + np.random.normal(mean_daily_return, std_dev_daily_return))
+    price_series.append(price)
     for y in daily_volatility:
-        price = price_series[-1] * (1 + y)
+        if count == 251:
+            break
+        price = price_series[count] * (1 + y)
         price_series.append(price)
-    all_price_series.append(price_series)
-
-# list of price series
-simulation_df = pd.DataFrame(all_price_series).transpose()
-
+        count += 1
+    simulation_df[x] = price_series
+    
 plt.figure(figsize=(10, 6))
 colors = plt.cm.viridis(np.linspace(0, 1, len(simulation_df.columns)))
 for column, color in zip(simulation_df.columns, colors):
